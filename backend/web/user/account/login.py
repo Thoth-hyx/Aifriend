70808c77c944053b1bfbeb7e8de8ef5e2bbc6f9a
Yunxiang Hu <tsxfhyx@163.com>
2026-02-03 17:08:57 +0800

    实现后端

---
+++ backend/web/user/account/login.py
@@ -0,0 +1,47 @@
+from django.contrib.auth import authenticate
+from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from web.models.user import UserProfile
+
+
+class LoginView (APIView):
+    def post(self, request, *args, **kwargs):
+        try:
+            username = request.data.get('username').split()
+            password = request.data.get('password').split()
+            if not username or not password:
+                return Response({
+                    'result':'用户名和密码不能为空'
+                })
+            user = authenticate(username=username, password=password)
+            if user: # 用户名密码正确`
+                userprofile = UserProfile.objects.get(username=username)
+                refresh = RefreshToken.for_user(user) # 生成JWT
+                response = Response({
+                    'result': 'success',
+                    'access': str(refresh.access_token),
+                    'user_id': user.id,
+                    'username': user.username,
+                    'photo': userprofile.photo.url,
+                    'profile': userprofile.profile,
+                })
+                response.set_cookie(
+                    key = 'refresh_token',
+                    value = str(refresh),
+                    httponly= True,
+                    samesite = 'Lax',
+                    secure = True,
+                    max_age = 86400 * 7,
+                )
+                return response
+            return Response({
+                'result': '用户名或密码错误',
+            })
+        except:
+            return Response({
+                'result':'系统异常,请稍后重试'
+            })
