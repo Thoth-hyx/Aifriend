c83fb73c038bf5de4cc1dce2f844d0c6b989f599
Yunxiang Hu <tsxfhyx@163.com>
2026-02-06 17:29:51 +0800

    实现完创建虚拟角色前端

---
+++ frontend/src/views/create/character/components/Profile.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {ref, watch} from "vue";
+
+const props = defineProps(['profile'])
+const myProfile = ref(props.profile)
+
+watch(()=>props.profile,newVal =>{
+  myProfile.value = newVal
+})
+
+defineExpose({
+  myProfile,
+})
+
+</script>
+
+<template>
+  <fieldser class="fieldset">
+    <label class="label text-base">角色介绍</label>
+    <textarea v-model="myProfile" rows="6" class="textarea w-108"></textarea>
+  </fieldser>
+</template>
+
+<style scoped>
+
+</style>
